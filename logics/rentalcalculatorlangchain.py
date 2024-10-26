03ba2c582d4656f58f91aa07695f52f133b56513
Cabbie-UK <67590705+Cabbie-UK@users.noreply.github.com>
2024-10-26 23:25:40 +0800

    add pysqlite3-binary==0.5.4

--- logics/rentalcalculatorlangchain.py
+++ logics/rentalcalculatorlangchain.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 # langchain_rental.py
 from typing import List, Dict
 from langchain.prompts import PromptTemplate
